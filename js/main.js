b9a62b81cbdb6340eb95b872adb14d1c102381f3
tfaaguilar <113370984+tfaaguilar@users.noreply.github.com>
2024-01-24 21:16:41 -0500

    Characters

--- js/main.js
+++ js/main.js
@@ -9,7 +9,23 @@
         fetch (`${baseURL}people`)
         .then(response => response.json())
         .then(function(response){
-            console.log(response.results);
+            // console.log(response.results);
+            const character = response.results;
+            const ul = document.createElement('ul');
+
+            character.forEach(character => {
+                console.log(character['name']);
+                console.log(character['created']);
+                const li = document.createElement('li'); 
+                const a = document.createElement('a');
+                a.textContent = character['name'];
+                li.appendChild(a);
+                ul.appendChild(li);           
+             });
+
+             characterBox.appendChild(ul);
+
+            //  video 65
         })
         .then()
         .catch();
